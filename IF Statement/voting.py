873b999b35eeec24287e613baa5611e487b97596
Packooo <wijayantoaw2@gmail.com>
2026-03-03 13:19:18 +0700

    page 75

---
+++ IF Statement/voting.py
@@ -0,0 +1,7 @@
+age = 20
+if age >= 18:
+    print("You are eligible to vote.")
+    print("Have you registered to vote?")
+else:
+    print("You are not eligible to vote.")
+    print("Please register to vote when you turn 18.")
